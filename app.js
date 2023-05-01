6b3116465bc5e0a5b2bee6c365f57bb99ed2c44e
Alexey Lagun <alexey.lagun@gmail.com>
2023-05-02 01:49:37 +0300

    fix: add Eslint & use rule (Tue, May 2, 2023 01:49:37 AM)

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 async function runApp() {
-  localStorage.setItem("lang", localStorage.getItem("lang") || "en");
-  let mode = "normal";
+  localStorage.setItem('lang', localStorage.getItem('lang') || 'en');
+  let mode = 'normal';
 
   const keyboardHTML = `
 <div class="keyboard_wrapper">
@@ -215,102 +215,102 @@ async function runApp() {
 </div>
 </div>`;
   const en = {
-    lang: "en",
+    lang: 'en',
     normal: [
-      "Esc 1 2 3 4 5 6 7 8 9 0 - = bksp",
-      "tab q w e r t y u i o p [ ] \\",
-      "caps a s d f g h j k l ; , enter",
-      "shift z x c v b n m , . / ᐃ shift",
-      "ctrl alt ` space en alt ᐊ ᐁ ᐅ del",
+      'Esc 1 2 3 4 5 6 7 8 9 0 - = bksp',
+      'tab q w e r t y u i o p [ ] \\',
+      'caps a s d f g h j k l ; , enter',
+      'shift z x c v b n m , . / ᐃ shift',
+      'ctrl alt ` space en alt ᐊ ᐁ ᐅ del',
     ],
     shift: [
-      "Esc ! @ # $ % ^ & * ( ) _ + bksp",
-      "tab Q W E R T Y U I O P { } |",
+      'Esc ! @ # $ % ^ & * ( ) _ + bksp',
+      'tab Q W E R T Y U I O P { } |',
       'caps A S D F G H J K L : " enter',
-      "shift Z X C V B N M < > ? ᐃ shift",
-      "ctrl alt ` space en alt ᐊ ᐁ ᐅ del",
+      'shift Z X C V B N M < > ? ᐃ shift',
+      'ctrl alt ` space en alt ᐊ ᐁ ᐅ del',
     ],
     caps: [
-      "Esc 1 2 3 4 5 6 7 8 9 0 - = bksp",
-      "tab Q W E R T Y U I O P [ ] \\",
-      "caps A S D F G H J K L ; , enter",
-      "shift Z X C V B N M , . / ᐃ shift",
-      "ctrl alt ` space en alt ᐊ ᐁ ᐅ del",
+      'Esc 1 2 3 4 5 6 7 8 9 0 - = bksp',
+      'tab Q W E R T Y U I O P [ ] \\',
+      'caps A S D F G H J K L ; , enter',
+      'shift Z X C V B N M , . / ᐃ shift',
+      'ctrl alt ` space en alt ᐊ ᐁ ᐅ del',
     ],
     capsOnShift: [
-      "Esc ! @ # $ % ^ & * ( ) _ + bksp",
-      "tab q w e r t y u i o p { } |",
+      'Esc ! @ # $ % ^ & * ( ) _ + bksp',
+      'tab q w e r t y u i o p { } |',
       'caps a s d f g h j k l : " enter',
-      "shift z x c v b n m < > ? ᐃ shift",
-      "ctrl alt ` space en alt ᐊ ᐁ ᐅ del",
+      'shift z x c v b n m < > ? ᐃ shift',
+      'ctrl alt ` space en alt ᐊ ᐁ ᐅ del',
     ],
   };
   const ru = {
-    lang: "ru",
+    lang: 'ru',
     normal: [
-      "Esc 1 2 3 4 5 6 7 8 9 0 - = bksp",
-      "tab й ц у к е н г ш щ з х ъ \\",
-      "caps ф ы в а п р о л д ж э enter",
-      "shift я ч с м и т ь б ю . ᐃ shift",
-      "ctrl alt ё space ru alt ᐊ ᐁ ᐅ del",
+      'Esc 1 2 3 4 5 6 7 8 9 0 - = bksp',
+      'tab й ц у к е н г ш щ з х ъ \\',
+      'caps ф ы в а п р о л д ж э enter',
+      'shift я ч с м и т ь б ю . ᐃ shift',
+      'ctrl alt ё space ru alt ᐊ ᐁ ᐅ del',
     ],
     shift: [
       'Esc ! " № ; % : ? * ( ) _ + bksp',
-      "tab Й Ц У К Е Н Г Ш Щ З Х Ъ /",
-      "caps Ф Ы В А П Р О Л Д Ж Э enter",
-      "shift Я Ч С М И Т Ь Б Ю , ᐃ shift",
-      "ctrl alt Ё space ru alt ᐊ ᐁ ᐅ del",
+      'tab Й Ц У К Е Н Г Ш Щ З Х Ъ /',
+      'caps Ф Ы В А П Р О Л Д Ж Э enter',
+      'shift Я Ч С М И Т Ь Б Ю , ᐃ shift',
+      'ctrl alt Ё space ru alt ᐊ ᐁ ᐅ del',
     ],
     caps: [
-      "Esc 1 2 3 4 5 6 7 8 9 0 - = bksp",
-      "tab Й Ц У К Е Н Г Ш Щ З Х Ъ \\",
-      "caps Ф Ы В А П Р О Л Д Ж Э enter",
-      "shift Я Ч С М И Т Ь Б Ю . ᐃ shift",
-      "ctrl alt Ё space ru alt ᐊ ᐁ ᐅ del",
+      'Esc 1 2 3 4 5 6 7 8 9 0 - = bksp',
+      'tab Й Ц У К Е Н Г Ш Щ З Х Ъ \\',
+      'caps Ф Ы В А П Р О Л Д Ж Э enter',
+      'shift Я Ч С М И Т Ь Б Ю . ᐃ shift',
+      'ctrl alt Ё space ru alt ᐊ ᐁ ᐅ del',
     ],
     capsOnShift: [
       'Esc ! " № ; % : ? * ( ) _ + bksp',
-      "tab й ц у к е н г ш щ з х ъ /",
-      "caps ф ы в а п р о л д ж э enter",
-      "shift я ч с м и т ь б ю , ᐃ shift",
-      "ctrl alt ё space ru alt ᐊ ᐁ ᐅ del",
+      'tab й ц у к е н г ш щ з х ъ /',
+      'caps ф ы в а п р о л д ж э enter',
+      'shift я ч с м и т ь б ю , ᐃ shift',
+      'ctrl alt ё space ru alt ᐊ ᐁ ᐅ del',
     ],
   };
 
   // вставляем каркас будущей клавы
-  const virtualKeyboard = document.getElementById("virtual__keyboard");
-  virtualKeyboard.insertAdjacentHTML("afterbegin", keyboardHTML);
+  const virtualKeyboard = document.getElementById('virtual__keyboard');
+  virtualKeyboard.insertAdjacentHTML('afterbegin', keyboardHTML);
 
   // проверка на допустимые клавиши
   const allowKey = [];
   document
-    .querySelectorAll(".key")
+    .querySelectorAll('.key')
     .forEach((e) => allowKey.push(e.dataset.code));
 
   // определение языка
-  const currentLangObj = () => {
-    return localStorage.getItem("lang") === "en" ? en : ru;
-  };
+  const currentLangObj = () => { if (localStorage.getItem('lang') === 'en') { return en; } return ru; };
+  let lang = currentLangObj();
 
   // нажатые клавиши
   const pressedKeys = {};
 
   // фокусровка
   const focusArea = () => {
-    const textarea = document.querySelector(".display");
+    const textarea = document.querySelector('.display');
     textarea.focus();
   };
 
   // отрисовка клавиатуры
-  const createKeyboard = (lang = "en", mode) => {
-    let rowArr = document.querySelectorAll(".row");
-    let currObj = currentLangObj(lang);
-    currObj[mode].forEach((el, ind) => {
-      let symbolArr = el.split(" ");
+  const createKeyboard = (language, modification) => {
+    const rowArr = document.querySelectorAll('.row');
+    const currObj = currentLangObj();
+    currObj[modification].forEach((el, ind) => {
+      const symbolArr = el.split(' ');
+      // eslint-disable-next-line no-plusplus
       for (let i = 0; i < symbolArr.length; i++) {
         rowArr[ind].children[i].firstElementChild.setAttribute(
-          "data-content",
-          symbolArr[i]
+          'data-content',
+          symbolArr[i],
         );
         rowArr[ind].children[i].firstElementChild.innerHTML = symbolArr[i];
       }
@@ -319,41 +319,41 @@ async function runApp() {
 
   // обработчик кликов - нажатие
   const mouseClickDown = (event) => {
-    const key = event.target.closest(".key");
+    const key = event.target.closest('.key');
     if (!key) return;
-    key.classList.add("active");
+    key.classList.add('active');
 
-    if (key.id === "lang") {
-      if (localStorage.getItem("lang") === "en") {
-        localStorage.setItem("lang", "ru");
+    if (key.id === 'lang') {
+      if (localStorage.getItem('lang') === 'en') {
+        localStorage.setItem('lang', 'ru');
         lang = currentLangObj();
         createKeyboard(lang, mode);
       } else {
-        localStorage.setItem("lang", "en");
+        localStorage.setItem('lang', 'en');
         lang = currentLangObj();
         createKeyboard(lang, mode);
       }
     }
-    if (key.id === "caps") {
-      if (mode != "caps") {
-        key.classList.toggle("caps-lock__active");
-        mode = "caps";
+    if (key.id === 'caps') {
+      if (mode !== 'caps') {
+        key.classList.toggle('caps-lock__active');
+        mode = 'caps';
         lang = currentLangObj();
         createKeyboard(lang, mode);
       } else {
-        key.classList.toggle("caps-lock__active");
-        mode = "normal";
+        key.classList.toggle('caps-lock__active');
+        mode = 'normal';
         lang = currentLangObj();
         createKeyboard(lang, mode);
       }
     }
-    if (key.id === "shift-left" || key.id === "shift-right") {
-      if (mode === "normal") {
-        mode = "shift";
+    if (key.id === 'shift-left' || key.id === 'shift-right') {
+      if (mode === 'normal') {
+        mode = 'shift';
         lang = currentLangObj();
         createKeyboard(lang, mode);
-      } else if (mode === "caps") {
-        mode = "capsOnShift";
+      } else if (mode === 'caps') {
+        mode = 'capsOnShift';
         lang = currentLangObj();
         createKeyboard(lang, mode);
       }
@@ -362,16 +362,16 @@ async function runApp() {
 
   // обработчик кликов - отпускание
   const mouseClickUp = (event) => {
-    const key = event.target.closest(".key");
+    const key = event.target.closest('.key');
     if (!key) return;
-    key.classList.remove("active");
-    if (key.id === "shift-left" || key.id === "shift-right") {
-      if (mode === "shift") {
-        mode = "normal";
+    key.classList.remove('active');
+    if (key.id === 'shift-left' || key.id === 'shift-right') {
+      if (mode === 'shift') {
         lang = currentLangObj();
         createKeyboard(lang, mode);
-      } else if (mode === "capsOnShift") {
-        mode = "shift";
+        createKeyboard(mode);
+      } else if (mode === 'capsOnShift') {
+        mode = 'shift';
         lang = currentLangObj();
         createKeyboard(lang, mode);
       }
@@ -380,57 +380,53 @@ async function runApp() {
 
   // обработчик кликов - ключ
   const mouseClickKey = (event) => {
-    const key = event.target.closest(".key");
+    const key = event.target.closest('.key');
 
     if (!key) return;
     focusArea();
-    let cursor = textarea.selectionStart;
+    const textarea = document.querySelector('.display');
+    const cursor = textarea.selectionStart;
 
     if (/Escape|CapsLock|Shift|Control|Alt|Lang/.test(key.dataset.code)) {
       return;
     }
 
     if (/Backspace/.test(key.dataset.code)) {
-      textarea.value =
-        textarea.value.slice(0, cursor - 1) + textarea.value.slice(cursor);
+      textarea.value = textarea.value.slice(0, cursor - 1) + textarea.value.slice(cursor);
       textarea.setSelectionRange(cursor - 1, cursor - 1);
       return;
     }
 
     if (/Delete/.test(key.dataset.code)) {
-      textarea.value =
-        textarea.value.slice(0, cursor) + textarea.value.slice(cursor + 1);
+      textarea.value = textarea.value.slice(0, cursor) + textarea.value.slice(cursor + 1);
       textarea.setSelectionRange(cursor, cursor);
       return;
     }
 
     if (/Enter/.test(key.dataset.code)) {
-      textarea.value =
-        textarea.value.slice(0, cursor) + "\n" + textarea.value.slice(cursor);
+      textarea.value = `${textarea.value.slice(0, cursor)}\n${textarea.value.slice(cursor)}`;
       textarea.setSelectionRange(cursor + 1, cursor + 1);
       return;
     }
 
     if (/Space/.test(key.dataset.code)) {
-      textarea.value =
-        textarea.value.slice(0, cursor) + " " + textarea.value.slice(cursor);
+      textarea.value = `${textarea.value.slice(0, cursor)} ${textarea.value.slice(cursor)}`;
       textarea.setSelectionRange(cursor + 1, cursor + 1);
       return;
     }
 
     if (/Tab/.test(key.dataset.code)) {
-      textarea.value =
-        textarea.value.slice(0, cursor) + "\t" + textarea.value.slice(cursor);
+      textarea.value = `${textarea.value.slice(0, cursor)}\t${textarea.value.slice(cursor)}`;
       textarea.setSelectionRange(cursor + 1, cursor + 1);
       return;
     }
 
     if (/ArrowUp/.test(key.dataset.code)) {
-      let arrStr = textarea.value.split("\n").map((e) => e.length);
+      const arrStr = textarea.value.split('\n').map((e) => e.length);
       let sumLenStr = 0;
-      for (let strNumber = 0; strNumber < arrStr.length; strNumber++) {
-        sumLenStr +=
-          strNumber + 1 < arrStr.length
+      for (let strNumber = 0; strNumber < arrStr.length; strNumber += 1) {
+        sumLenStr
+          += strNumber + 1 < arrStr.length
             ? arrStr[strNumber] + 1
             : arrStr[strNumber];
         if (cursor <= arrStr[0]) {
@@ -438,17 +434,17 @@ async function runApp() {
           break;
         } else if (cursor <= sumLenStr) {
           if (
-            cursor - (sumLenStr - arrStr[strNumber] - 1) >
-            arrStr[strNumber - 1]
+            cursor - (sumLenStr - arrStr[strNumber] - 1)
+            > arrStr[strNumber - 1]
           ) {
             textarea.setSelectionRange(
               cursor - (cursor - (sumLenStr - arrStr[strNumber] - 1)) - 1,
-              cursor - (cursor - (sumLenStr - arrStr[strNumber] - 1)) - 1
+              cursor - (cursor - (sumLenStr - arrStr[strNumber] - 1)) - 1,
             );
           } else {
             textarea.setSelectionRange(
               cursor - arrStr[strNumber - 1] - 1,
-              cursor - arrStr[strNumber - 1] - 1
+              cursor - arrStr[strNumber - 1] - 1,
             );
           }
           break;
@@ -459,38 +455,38 @@ async function runApp() {
     if (/ArrowLeft/.test(key.dataset.code)) {
       textarea.setSelectionRange(
         cursor !== 0 ? cursor - 1 : 0,
-        cursor !== 0 ? cursor - 1 : 0
+        cursor !== 0 ? cursor - 1 : 0,
       );
       return;
     }
     if (/ArrowDown/.test(key.dataset.code)) {
-      let arrStr = textarea.value.split("\n").map((e) => e.length);
+      const arrStr = textarea.value.split('\n').map((e) => e.length);
       let sumLenStr = 0;
-      for (let strNumber = 0; strNumber < arrStr.length; strNumber++) {
-        sumLenStr +=
-          strNumber + 1 < arrStr.length
+      for (let strNumber = 0; strNumber < arrStr.length; strNumber += 1) {
+        sumLenStr
+          += strNumber + 1 < arrStr.length
             ? arrStr[strNumber] + 1
             : arrStr[strNumber];
         if (cursor < sumLenStr) {
           if (strNumber + 1 === arrStr.length) {
             textarea.setSelectionRange(
               textarea.value.length,
-              textarea.value.length
+              textarea.value.length,
             );
             break;
           } else {
             if (
-              cursor - (sumLenStr - arrStr[strNumber] - 1) >
-              arrStr[strNumber + 1]
+              cursor - (sumLenStr - arrStr[strNumber] - 1)
+              > arrStr[strNumber + 1]
             ) {
               textarea.setSelectionRange(
                 sumLenStr + arrStr[strNumber + 1],
-                sumLenStr + arrStr[strNumber + 1]
+                sumLenStr + arrStr[strNumber + 1],
               );
             } else {
               textarea.setSelectionRange(
                 cursor + arrStr[strNumber] + 1,
-                cursor + arrStr[strNumber] + 1
+                cursor + arrStr[strNumber] + 1,
               );
             }
             break;
@@ -503,11 +499,8 @@ async function runApp() {
       textarea.setSelectionRange(cursor + 1, cursor + 1);
       return;
     }
-
-    textarea.value =
-      textarea.value.slice(0, cursor) +
-      key.firstElementChild.dataset.content +
-      textarea.value.slice(cursor);
+    const { oKey } = key.firstElementChild.dataset;
+    textarea.value = textarea.value.slice(0, cursor) + oKey.content + textarea.value.slice(cursor);
     textarea.setSelectionRange(cursor + 1, cursor + 1);
   };
 
@@ -520,72 +513,72 @@ async function runApp() {
     const curr = event;
     document
       .querySelector(`[data-code="${curr.code}"]`)
-      .classList.add("active");
-    let cursor = textarea.selectionStart;
-    if (curr.code === "Tab") {
+      .classList.add('active');
+    const textarea = document.querySelector('.display');
+    const cursor = textarea.selectionStart;
+    if (curr.code === 'Tab') {
       curr.preventDefault();
-      textarea.value =
-        textarea.value.slice(0, cursor) + "\t" + textarea.value.slice(cursor);
+      textarea.value = `${textarea.value.slice(0, cursor)}\t${textarea.value.slice(cursor)}`;
       textarea.setSelectionRange(cursor + 1, cursor + 1);
       return;
     }
-    if (curr.code === "AltLeft") {
+    if (curr.code === 'AltLeft') {
       pressedKeys[curr.code] = true;
       if (pressedKeys.ShiftLeft) {
-        if (localStorage.getItem("lang") === "en") {
-          localStorage.setItem("lang", "ru");
+        if (localStorage.getItem('lang') === 'en') {
+          localStorage.setItem('lang', 'ru');
           lang = currentLangObj();
           createKeyboard(lang, mode);
         } else {
-          localStorage.setItem("lang", "en");
+          localStorage.setItem('lang', 'en');
           lang = currentLangObj();
           createKeyboard(lang, mode);
         }
       }
       curr.preventDefault();
     }
-    if (curr.key === "Alt") {
+    if (curr.key === 'Alt') {
       curr.preventDefault();
     }
-    if (curr.code === "ShiftLeft") {
+    if (curr.code === 'ShiftLeft') {
       pressedKeys[curr.code] = true;
       if (pressedKeys.AltLeft) {
-        if (localStorage.getItem("lang") === "en") {
-          localStorage.setItem("lang", "ru");
+        if (localStorage.getItem('lang') === 'en') {
+          localStorage.setItem('lang', 'ru');
           lang = currentLangObj();
           createKeyboard(lang, mode);
         } else {
-          localStorage.setItem("lang", "en");
+          localStorage.setItem('lang', 'en');
           lang = currentLangObj();
           createKeyboard(lang, mode);
         }
       }
     }
-    if (curr.code === "CapsLock") {
-      if (mode === "normal") {
-        mode = "caps";
+    if (curr.code === 'CapsLock') {
+      if (mode === 'normal') {
+        mode = 'caps';
         document
           .querySelector(`[data-code="${curr.code}"]`)
-          .classList.toggle("caps-lock__active");
+          .classList.toggle('caps-lock__active');
         lang = currentLangObj();
         createKeyboard(lang, mode);
       } else {
-        mode = "normal";
+        mode = 'normal';
         document
           .querySelector(`[data-code="${curr.code}"]`)
-          .classList.remove("caps-lock__active");
+          .classList.remove('caps-lock__active');
         lang = currentLangObj();
         createKeyboard(lang, mode);
       }
     }
-    if (curr.code === "ShiftLeft" || curr.code === "ShiftRight") {
+    if (curr.code === 'ShiftLeft' || curr.code === 'ShiftRight') {
       pressedKeys[curr.code] = true;
-      if (mode === "normal" || mode === "shift") {
-        mode = "shift";
+      if (mode === 'normal' || mode === 'shift') {
+        mode = 'shift';
         lang = currentLangObj();
         createKeyboard(lang, mode);
-      } else if (mode === "caps") {
-        mode = "capsOnShift";
+      } else if (mode === 'caps') {
+        mode = 'capsOnShift';
         lang = currentLangObj();
         createKeyboard(lang, mode);
       }
@@ -601,20 +594,20 @@ async function runApp() {
     const curr = event;
     document
       .querySelector(`[data-code="${curr.code}"]`)
-      .classList.remove("active");
-    if (curr.code === "AltLeft") {
+      .classList.remove('active');
+    if (curr.code === 'AltLeft') {
       delete pressedKeys[curr.code];
       curr.preventDefault();
     }
-    if (curr.key === "Shift") {
+    if (curr.key === 'Shift') {
       if (pressedKeys.ShiftLeft || pressedKeys.ShiftRight) {
-        if (mode === "capsOnShift") {
-          mode = "caps";
+        if (mode === 'capsOnShift') {
+          mode = 'caps';
           lang = currentLangObj();
           createKeyboard(lang, mode);
           delete pressedKeys[curr.code];
-        } else if (mode === "shift") {
-          mode = "normal";
+        } else if (mode === 'shift') {
+          mode = 'normal';
           lang = currentLangObj();
           createKeyboard(lang, mode);
           delete pressedKeys[curr.code];
@@ -623,17 +616,17 @@ async function runApp() {
     }
   };
 
-  const keyboard = document.querySelector(".keys");
-  keyboard.addEventListener("mousedown", mouseClickDown);
-  keyboard.addEventListener("mouseup", mouseClickUp);
-  keyboard.addEventListener("click", mouseClickKey);
-  document.addEventListener("keydown", handClickDown);
-  document.addEventListener("keyup", handClickUp);
+  const keyboard = document.querySelector('.keys');
+  keyboard.addEventListener('mousedown', mouseClickDown);
+  keyboard.addEventListener('mouseup', mouseClickUp);
+  keyboard.addEventListener('click', mouseClickKey);
+  document.addEventListener('keydown', handClickDown);
+  document.addEventListener('keyup', handClickUp);
 
   createKeyboard(lang, mode);
 
   // фокус на дисплее
-  const textarea = document.querySelector(".display");
+  const textarea = document.querySelector('.display');
   textarea.focus();
 }
 
